c634e8f5abe02786561416deb431e33704778ff4
colin <liangml4@lenovo.com>
2025-09-22 16:22:45 +0800

    add

---
+++ LibtorchSimpleNeuralNetwork/LibtorchSimpleNeuralNetwork.cpp
@@ -0,0 +1,12 @@
+﻿// LibtorchSimpleNeuralNetwork.cpp: 定义应用程序的入口点。
+//
+#include "torch/torch.h"
+#include "LibtorchSimpleNeuralNetwork.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
