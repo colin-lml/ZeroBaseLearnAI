6de68d94e651c0db56faa53050c01532800cf72c
qq00769539 <qq00769539@noreply.gitcode.com>
2025-09-24 22:22:40 +0800

    add

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 #include "torch/torch.h"
-
+#if 0
 #include <io.h>
 void load_data_from_folder(std::string path, std::string type, std::vector<std::string>& list_images, std::vector<int>& list_labels, int label);
 
@@ -60,15 +60,18 @@ void load_data_from_folder(std::string path, std::string type, std::vector<std::
     } while (_findnext(hFile, &fileInfo) == 0);
     return;
 }
-class myDataset :public torch::data::Dataset<myDataset> {
+class myDataset :public torch::data::Dataset<myDataset> 
+{
 public:
     int num_classes = 0;
-    myDataset(std::string image_dir, std::string type) {
+    myDataset(std::string image_dir, std::string type) 
+    {
         // 调用遍历文件的函数
         load_data_from_folder(image_dir, std::string(type), image_paths, labels, num_classes);
     }
     // 重写 get()，根据传入的index来获得指定的数据
-    torch::data::Example<> get(size_t index) override {
+    torch::data::Example<> get(size_t index) override 
+    {
         std::string image_path = image_paths.at(index);// 根据index得到指定的图像
         cv::Mat image = cv::imread(image_path);// 读取图像
         cv::resize(image, image, cv::Size(224, 224));// 调整大小，使得尺寸统一，用于张量stack
@@ -89,11 +92,51 @@ private:
     std::vector<int> labels;// 所有图像的类别
 };
 
+#endif
 
 
+struct Net : torch::nn::Module
+{
+    Net()
+    {
+       auto l = torch::nn::Linear(2, 2);
+       auto l2 = torch::nn::Linear(2, 2);
+
+       l->weight.data() = torch::tensor({ {0.15,0.25}
+                                          ,{0.2,0.3} });
+                                        
+
+       l->bias.data() = torch::tensor({0.35,0.35});
+
+       l2->weight.data() = torch::tensor({ {0.4,0.5}
+                                           ,{0.45,0.55} });
+                                       
+
+       l2->bias.data() = torch::tensor({ 0.6,0.6 });
+       
+       //std::cout << "l:" << l->weight << std::endl;
+      // std::cout << "l2:" << l2->weight << std::endl;
+
+       fc1 = register_module("fc1", l);
+       fc2 = register_module("fc2", l2);
+
+    }
+
+    torch::Tensor forward(torch::Tensor x)
+    {
+        x = torch::sigmoid(fc1->forward(x));
+
+        x = torch::sigmoid(fc2->forward(x));
+
+        return x;
+    }
+
+    torch::nn::Linear fc1{ nullptr }, fc2{ nullptr };
+};
 
 int main()
 {
+    /*
     auto batch_size = 1;
     auto mydataset = myDataset("D:\\hymenoptera_data\\", ".jpg").map(torch::data::transforms::Stack<>());
     auto mdataloader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(
@@ -105,13 +148,18 @@ int main()
         auto target = batch.target;
         std::cout << data.sizes() << target << std::endl;
     }
+ */
 
-
-    torch::Tensor a = torch::tensor({ {1, 2}, {3, 4} }, torch::dtype(torch::kFloat32));
-    torch::Tensor b = torch::tensor({ {5, 6}, {7, 8} }, torch::dtype(torch::kFloat32));
-    torch::Tensor c = a + b;
-    std::cout << "xxx:\n" << c << std::endl;
-
+    //torch::Tensor a = torch::tensor(4, 8, torch::dtype(torch::kFloat32));
+    //torch::Tensor b = torch::tensor({ {5, 6}, {7, 8} }, torch::dtype(torch::kFloat32));
+    //torch::Tensor c = a + b;
+    //std::cout << "xxx:\n" << c << std::endl;
+   
+    torch::Tensor input = torch::tensor({ { 0.05, 0.1 } });
+   // std::cout << "xxx:\n" << input << std::endl<< input.sizes()<< std::endl;
+    Net m;
+    torch::Tensor v = m.forward(input);
+    std::cout << "r :\n" << v << std::endl;
 
     std::cin.get();
 
