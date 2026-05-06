368552eb83ee84ab706b0b578577d1b6ef8ed8f3
qq00769539 <qq00769539@noreply.gitcode.com>
2026-05-06 21:59:24 +0800

    ok

--- GpuMakeProject/GpuMakeProject.cpp
+++ GpuMakeProject/GpuMakeProject.cpp
@@ -23,7 +23,7 @@ public:
     }
 
 
-    torch::Tensor forward(torch::Tensor x)
+    torch::Tensor forward(torch::Tensor& x)
     {
 
         x = m_emb->forward(x);
@@ -62,12 +62,12 @@ TORCH_MODULE(EmbeddingWithPosition);
 
 struct DeOnlyOptions
 {
-    int64_t dmodel;
-    int64_t head;
-    int64_t ffn;
-    int64_t layers;
-    int64_t vocab_size;
-    int64_t max_len;
+    int64_t dmodel = 512;
+    int64_t head = 8;
+    int64_t ffn = 2048;
+    int64_t layers = 6;
+    int64_t vocab_size = 300;
+    int64_t max_len = 500;
 };
 
 
@@ -98,6 +98,36 @@ public:
         //torch::nn::TransformerEncoderLayer()
     }
 
+    auto forward(torch::Tensor x)
+    {
+        // x [bath, seq]
+        int64_t batch = x.size(0);
+        int64_t seq = x.size(1);
+        auto src_mask = generate_square_subsequent_mask(seq);
+        x = m_emb->forward(x);
+        
+        x = x.permute({ 1,0, 2 });
+       // cout <<x.sizes()<< endl;
+
+        for (auto& item :  *moduleLayers)
+        {
+            x = item->as<torch::nn::TransformerEncoderLayer>()->forward(x, src_mask);
+            //std::cout << "tgt\n" << tgt.sizes() << std::endl;
+            //tgt = item->as<torch::nn::TransformerEncoderLayer>()->forward(tgt);
+        }
+
+        return fc->forward(x);
+    }
+
+    torch::Tensor generate_square_subsequent_mask(int64_t sz)
+    {
+        auto mask = torch::triu(torch::ones({ sz, sz }, torch::kFloat32), 1);
+
+        mask = mask.masked_fill(mask == 1, -std::numeric_limits<float>::infinity());
+        return mask;
+    }
+
+
 public:
     EmbeddingWithPosition m_emb{ nullptr };
     torch::nn::ModuleList moduleLayers{ nullptr };
@@ -106,10 +136,73 @@ public:
 };
 TORCH_MODULE(DecodersOnly);
 
+#define max_train  1000*5
 
 int main()
 {
 	gDType = torch::cuda::is_available() ? torch::kCUDA : torch::kCPU;
+
+    double accuracy = 0.0003;
+
+    DeOnlyOptions opt;
+    opt.dmodel = 128;
+    opt.head = 4;
+    opt.ffn = 512;
+    opt.layers = 1;
+    opt.max_len = 800;
+    opt.vocab_size = 610;
+
+    float total_loss = 0;
+
+    DecodersOnly model(opt);
+
+    auto options = torch::nn::CrossEntropyLossOptions().ignore_index(0);
+    torch::nn::CrossEntropyLoss loss_fn(options);
+
+    torch::optim::Adam optimizer(model->parameters(), torch::optim::AdamOptions(1e-3));
+    model->train();
+    // s: 1, e: 2 , p: 0
+
+    vector<int64_t> in;
+    vector<int64_t> lab;
+    in.push_back(1);
+    for (int i=3;i<320;i++)
+    {
+        in.push_back(i);
+        lab.push_back(i);
+    }
+   lab.push_back(2);
+
+    auto input = torch::tensor(in, torch::kLong).unsqueeze(0);
+    auto lable = torch::tensor(lab, torch::kLong);
+    
+    cout << input.sizes()<< endl<<endl;
+    cout << lable.sizes() << endl;
+
+    for (int i = 0; i < max_train; i++)
+    {
+        auto output = model->forward(input);
+        output = output.reshape({ -1, output.size(2) });
+
+        auto loss = loss_fn(output, lable);
+       
+        torch::nn::utils::clip_grad_norm_(model->parameters(), 1.0);
+        loss.backward();
+        optimizer.step();
+
+        total_loss = loss.item<float>();
+
+        if (i % 10 == 0 || (i+1) == max_train)
+        {
+            cout << i + 1 << " , loss: " << total_loss << endl;
+        }
+        
+        if (total_loss < accuracy)
+        {
+            cout << i + 1 << " , loss: " << total_loss << " , end... " << endl;
+            break;
+        }
+    }
 
 	//auto input = torch::tensor({ 0.050,0.10 }, torch::kDouble).to(gDType);
 	///cout << input << endl;
