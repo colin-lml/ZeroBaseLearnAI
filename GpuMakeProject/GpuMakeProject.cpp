a496712f23a164d50d7ea62bf6efd30c022814bf
colin <liangml4@lenovo.com>
2026-05-06 17:20:31 +0800

    update

--- GpuMakeProject/GpuMakeProject.cpp
+++ GpuMakeProject/GpuMakeProject.cpp
@@ -25,11 +25,12 @@ public:
 
     torch::Tensor forward(torch::Tensor x)
     {
+
+        x = m_emb->forward(x);
         if ((x.dim() == 2))
         {
             x = x.unsqueeze_(-2);
         }
-
         auto dim = x.size(0);
 
         // std::cout <<"pos " << _posEncode.slice(0, 0, dim).sizes() << std::endl;
@@ -50,7 +51,7 @@ private:
 
     }
 
-public:
+
     torch::nn::Embedding m_emb{ nullptr };
     torch::Tensor m_posEncode;
     int64_t m_iDmodel = 0;
@@ -59,17 +60,59 @@ public:
 
 TORCH_MODULE(EmbeddingWithPosition);
 
+struct DeOnlyOptions
+{
+    int64_t dmodel;
+    int64_t head;
+    int64_t ffn;
+    int64_t layers;
+    int64_t vocab_size;
+    int64_t max_len;
+};
 
 
+class DecodersOnlyImpl : public torch::nn::Module
+{
 
+public:
+
+    DecodersOnlyImpl(const DeOnlyOptions&  inOpt)
+    {
+        fc = register_module("fc", torch::nn::Linear(inOpt.dmodel, inOpt.vocab_size));
+
+        m_emb = register_module("m_emb", EmbeddingWithPosition(inOpt.dmodel, inOpt.vocab_size, inOpt.max_len));
+        
+        moduleLayers = register_module("EncoderLayers", torch::nn::ModuleList());
+
+        m_option = inOpt;
+
+        for (int i = 0; i < inOpt.layers; i++)
+        {
+            torch::nn::TransformerEncoderLayerOptions opt(inOpt.dmodel, inOpt.head);
+            opt.dim_feedforward(inOpt.ffn);
+            //opt.dropout(0);
+      
+            moduleLayers->push_back(torch::nn::TransformerEncoderLayer(opt));
+        }
+
+        //torch::nn::TransformerEncoderLayer()
+    }
+
+public:
+    EmbeddingWithPosition m_emb{ nullptr };
+    torch::nn::ModuleList moduleLayers{ nullptr };
+    torch::nn::Linear fc{ nullptr };
+    DeOnlyOptions m_option;
+};
+TORCH_MODULE(DecodersOnly);
 
 
 int main()
 {
 	gDType = torch::cuda::is_available() ? torch::kCUDA : torch::kCPU;
 
-	auto input = torch::tensor({ 0.050,0.10 }, torch::kDouble).to(gDType);
-	cout << input << endl;
+	//auto input = torch::tensor({ 0.050,0.10 }, torch::kDouble).to(gDType);
+	///cout << input << endl;
 
 	cin.get();
 	return 0;
