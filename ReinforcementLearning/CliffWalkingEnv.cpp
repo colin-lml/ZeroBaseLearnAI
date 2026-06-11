290ba5d81a6b0668b66fcce0ca94e439634a74a8
colin <liangml4@lenovo.com>
2026-06-11 17:08:40 +0800

    av

---
+++ ReinforcementLearning/CliffWalkingEnv.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include "CliffWalkingEnv.h"
+
+
+CliffWalkingEnv::CliffWalkingEnv(int r, int c)
+{
+	m_nCol = c;
+	m_nRow = r;
+}
---
+++ ReinforcementLearning/CliffWalkingEnv.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class CliffWalkingEnv
+{
+public:
+	CliffWalkingEnv();
+
+private:
+	int m_nRow;
+	int m_nCol;
+};
+
