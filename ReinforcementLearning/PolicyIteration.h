c088e65922fbd8532e5c54de750884a4423f5f9e
colin <liangml4@lenovo.com>
2026-06-17 17:08:09 +0800

    d

--- ReinforcementLearning/PolicyIteration.h
+++ ReinforcementLearning/PolicyIteration.h
@@ -20,7 +20,7 @@ private:
 	vector<double> m_vecV;
 	ActionList m_2dPI;
 	const double m_dbTheta = 0.001;
-	//const double m_dbGamma = 0.9;
-	const double m_dbGamma = 1;
+	const double m_dbGamma = 0.9;
+	//const double m_dbGamma = 1;
 };
 
