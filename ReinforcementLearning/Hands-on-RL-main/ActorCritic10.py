f66344d791cbfa9aebc48d428a017b13af543950
colin <liangml4@lenovo.com>
2026-07-10 10:33:38 +0800

    rr

---
+++ ReinforcementLearning/Hands-on-RL-main/ActorCritic10.py
@@ -0,0 +1,197 @@
+import random
+import gymnasium as  gym
+import numpy as np
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+##import rl_utils
+from tqdm import tqdm
+
+
+class ReplayBuffer:
+    """经验回放"""
+    def __init__(self,batch_size=64):
+        self.data = []
+        self.batch_size = batch_size
+    
+    def add_data(self,state,action,reward,next_state,done):
+        self.data.append((state,action,reward,next_state,done))
+    
+    def sample(self):
+        sample_data = random.sample(self.data,self.batch_size)
+        state, action, reward, next_state, done = zip(*sample_data)
+        return np.array(state), np.array(action), np.array(reward), np.array(next_state), np.array(done)
+
+    def length(self):
+        return len(self.data)
+    
+    def size(self):
+        return len(self.data)
+
+
+
+
+
+class PolicyNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(PolicyNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return F.softmax(self.fc2(x), dim=1)
+    
+  
+class ValueNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim):
+        super(ValueNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+
+
+class ActorCritic:
+    def __init__(self, state_dim, hidden_dim, action_dim, actor_lr, critic_lr,
+                 gamma, device):
+        # 策略网络
+        self.actor = PolicyNet(state_dim, hidden_dim, action_dim).to(device)
+        self.critic = ValueNet(state_dim, hidden_dim).to(device)  # 价值网络
+        # 策略网络优化器
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),lr=actor_lr)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),lr=critic_lr)  # 价值网络优化器
+        self.gamma = gamma
+        self.device = device
+
+    def take_action(self, state):
+        state = torch.tensor([state], dtype=torch.float).to(self.device)
+        probs = self.actor(state)
+        action_dist = torch.distributions.Categorical(probs)
+        action = action_dist.sample()
+        return action.item()
+    
+    def predict_action(self, state):
+        
+        state = torch.tensor([state], dtype=torch.float).to(self.device)
+       
+        with torch.no_grad():
+            probs = self.actor(state)
+            action = torch.argmax(probs, dim=1)  
+        return action.item()
+
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states'], dtype=torch.float).to(self.device)
+        actions = torch.tensor(transition_dict['actions']).view(-1, 1).to(self.device)
+        rewards = torch.tensor(transition_dict['rewards'],dtype=torch.float).view(-1, 1).to(self.device)
+        next_states = torch.tensor(transition_dict['next_states'],dtype=torch.float).to(self.device)
+        dones = torch.tensor(transition_dict['dones'],dtype=torch.float).view(-1, 1).to(self.device)
+        ##print(f"states: {states.shape} , actions: {actions.shape} , rewards: {rewards.shape} , next_states: {next_states.shape} , dones: {dones.shape}")
+
+
+        # 时序差分目标
+        td_target = rewards + self.gamma * self.critic(next_states) * (1 - dones)
+        td_delta = td_target - self.critic(states)  # 时序差分误差
+        log_probs = torch.log(self.actor(states).gather(1, actions))
+        actor_loss = torch.mean(-log_probs * td_delta.detach())
+        # 均方误差损失函数
+        critic_loss = torch.mean( F.mse_loss(self.critic(states), td_target.detach()))
+        self.actor_optimizer.zero_grad()
+        self.critic_optimizer.zero_grad()
+        actor_loss.backward()  # 计算策略网络的梯度
+        critic_loss.backward()  # 计算价值网络的梯度
+        self.actor_optimizer.step()  # 更新策略网络的参数
+        self.critic_optimizer.step()  # 更新价值网络的参数
+
+
+
+def train_on_policy_agent(env, agent, num_episodes):
+    return_list = []
+    for i in range(10):
+        with tqdm(total=int(num_episodes/10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes/10)):
+                episode_return = 0
+                transition_dict = {'states': [], 'actions': [], 'next_states': [], 'rewards': [], 'dones': []}
+                state, info= env.reset()
+                done = False
+                count = 0 
+                while not done and count < 475:
+                    action = agent.take_action(state)
+                    next_state, reward, done, _,__ = env.step(action)
+                    transition_dict['states'].append(state)
+                    transition_dict['actions'].append(action)
+                    transition_dict['next_states'].append(next_state)
+                    transition_dict['rewards'].append(reward)
+                    transition_dict['dones'].append(done)
+                    state = next_state
+                    episode_return += reward
+                    count+=1
+
+                return_list.append(episode_return)
+                agent.update(transition_dict)
+                if (i_episode+1) % 10 == 0:
+                    pbar.set_postfix({'episode': '%d' % (num_episodes/10 * i + i_episode+1), 'return': '%.3f' % np.mean(return_list[-10:])})
+                pbar.update(1)
+    return return_list
+
+def moving_average(a, window_size):
+    cumulative_sum = np.cumsum(np.insert(a, 0, 0)) 
+    middle = (cumulative_sum[window_size:] - cumulative_sum[:-window_size]) / window_size
+    r = np.arange(1, window_size-1, 2)
+    begin = np.cumsum(a[:window_size-1])[::2] / r
+    end = (np.cumsum(a[:-window_size:-1])[::2] / r)[::-1]
+    return np.concatenate((begin, middle, end))
+
+
+def policy_agent_predict(env, agent):
+    episode_return = 0
+    state, info= env.reset()
+    done = False
+    count = 0
+    while not done and count < 600:
+        action = agent.predict_action(state)
+        next_state, reward, done, _,__ = env.step(action)
+        state = next_state
+        episode_return += reward
+        count += 1
+    return episode_return
+
+
+actor_lr = 1e-3
+critic_lr = 1e-2
+num_episodes = 1000
+hidden_dim = 128
+gamma = 0.98
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+env_name = 'CartPole-v1'
+env = gym.make(env_name)
+
+torch.manual_seed(0)
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.n
+agent = ActorCritic(state_dim, hidden_dim, action_dim, actor_lr, critic_lr,gamma, device)
+
+return_list = train_on_policy_agent(env, agent, num_episodes)
+
+test_return = policy_agent_predict(env, agent)
+
+print(f"#### Test return: {test_return}\n")
+
+
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('Actor-Critic on {}'.format(env_name))
+plt.show()
+
+mv_return = moving_average(return_list, 9)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('Actor-Critic on {}'.format(env_name))
+plt.show()
+
