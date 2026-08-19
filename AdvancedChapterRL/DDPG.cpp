5d876067a3d309c091ce8178f2786a3aafcc7e11
colin <liangml4@lenovo.com>
2026-08-19 15:56:56 +0800

    add ddpg

---
+++ AdvancedChapterRL/DDPG.cpp
@@ -0,0 +1,89 @@
+﻿#include "pch.h"
+#include "DDPG.h"
+
+double DDPG::TakeAction(VectorDouble& s0,bool bPredict)
+{
+    torch::NoGradGuard noGrad;
+
+    auto s = VectorDoubleTensor(s0, m_device);
+    auto actionTensor = m_actor->forward(s);
+
+    auto action = actionTensor.squeeze().item<double>();
+
+    if (!bPredict)
+    {
+        auto noise = torch::randn({ m_objEnv->GetActionDim()}, m_device) * m_dbSigma;
+        //cout << noise<<endl;
+        action = action + noise.squeeze().item<double>();
+    }
+
+    action = std::clamp(action,m_objEnv->GetActionLow(),m_objEnv->GetActionHigh());
+
+    return action;
+}
+
+void DDPG::GenerateTrainData(int maxCount)
+{
+    cout << "Currently DDPG" << endl;
+
+    m_stateDim = m_objEnv->GetStateDim();
+    m_actionDim = m_objEnv->GetActionDim();
+	auto actionBound = m_objEnv->GetActionHigh();
+    m_actor = PolicyNetCont(m_stateDim, m_actionDim, actionBound);
+    m_targetActor = PolicyNetCont(m_stateDim, m_actionDim, actionBound);
+
+    m_critic = QValueNetCont(m_stateDim, m_actionDim);
+    m_targetCritic = QValueNetCont(m_stateDim, m_actionDim);
+
+    m_actor->to(m_device);
+    m_targetActor->to(m_device);
+
+    m_critic->to(m_device);
+    m_targetCritic->to(m_device);
+
+    CopyModuleParameters(*m_actor, *m_targetActor);
+    CopyModuleParameters(*m_critic, *m_targetCritic);
+
+    m_actorOptimizer = std::make_unique<torch::optim::Adam>(m_actor->parameters(),torch::optim::AdamOptions(m_actorLearningRate));
+    m_criticOptimizer = std::make_unique<torch::optim::Adam>(m_critic->parameters(),torch::optim::AdamOptions(m_criticLearningRate));
+
+    m_actor->train();
+    m_critic->train();
+
+    m_targetActor->eval();
+    m_targetCritic->eval();
+
+    BaseAdvanced::GenerateTrainData(maxCount);
+
+    m_actor->eval();
+    m_critic->eval();
+
+    m_actorOptimizer.reset();
+    m_criticOptimizer.reset();
+}
+
+void DDPG::TrainGenerateItem1(const QwItem& item)
+{
+    AddCartPoleDataList(item);
+}
+void DDPG::TrainGenerateItem2(const QwList& vList)
+{
+
+}
+
+void DDPG::SoftUpdate(torch::nn::Module& source,torch::nn::Module& target)
+{
+    torch::NoGradGuard noGrad;
+
+    auto sourceParameters = source.parameters();
+    auto targetParameters = target.parameters();
+
+    TORCH_CHECK(sourceParameters.size() == targetParameters.size(),"DDPG::SoftUpdate: parameter count mismatch");
+
+    for (size_t i = 0; i < sourceParameters.size(); ++i)
+    {
+        targetParameters[i].mul_(1.0 - m_tau);
+        targetParameters[i].add_(sourceParameters[i],m_tau);
+    }
+}
+
---
+++ AdvancedChapterRL/DDPG.h
@@ -0,0 +1,107 @@
+﻿#pragma once
+
+class PolicyNetContImpl : public torch::nn::Module
+{
+public:
+
+    PolicyNetContImpl() = default;
+    PolicyNetContImpl(int64_t input,int64_t output, double actionBound, int64_t hidden = 128)
+    {
+        m_fc1 = register_module("fc1",torch::nn::Linear(input, hidden));
+        m_fc2 = register_module("fc2",torch::nn::Linear(hidden, output));
+        m_dbActionBound = actionBound;
+    }
+
+    torch::Tensor forward(torch::Tensor x)
+    {
+        x = torch::relu(m_fc1->forward(x));
+       
+        return m_dbActionBound * torch::tanh(m_fc2->forward(x));
+    }
+
+private:
+    torch::nn::Linear m_fc1{ nullptr };
+    torch::nn::Linear m_fc2{ nullptr };
+    double m_dbActionBound = 2.0;
+};
+
+TORCH_MODULE(PolicyNetCont);
+
+
+class QValueNetContImpl : public torch::nn::Module
+{
+public:
+    QValueNetContImpl() = default;
+
+    QValueNetContImpl(int64_t input,int64_t output,int64_t hidden = 128)
+    {
+        m_fc1 = register_module("fc1", torch::nn::Linear(input + output, hidden));
+
+        m_fc2 = register_module("fc2",torch::nn::Linear(hidden, hidden));
+
+        m_output = register_module("output",torch::nn::Linear(hidden, 1));
+    }
+
+    torch::Tensor forward( const torch::Tensor& state,const torch::Tensor& action)
+    {
+        auto x = torch::cat({ state, action }, 1);
+
+        x = torch::relu(m_fc1->forward(x));
+        x = torch::relu(m_fc2->forward(x));
+
+        return m_output->forward(x);
+    }
+
+private:
+    torch::nn::Linear m_fc1{ nullptr };
+    torch::nn::Linear m_fc2{ nullptr };
+    torch::nn::Linear m_output{ nullptr };
+};
+
+TORCH_MODULE(QValueNetCont);
+
+
+class DDPG : public BaseAdvanced
+{
+public:
+    DDPG(): BaseAdvanced(false){}
+private:
+    double TakeAction(VectorDouble& s0, bool bPredict = false) override;
+
+    void GenerateTrainData(int maxCount) override;
+
+    void TrainGenerateItem1(const QwItem& item) override;
+
+    void TrainGenerateItem2(const QwList& vList) override;
+
+
+    void SoftUpdate(torch::nn::Module& source,torch::nn::Module& target);
+
+
+private:
+    PolicyNetCont m_actor{ nullptr };
+    PolicyNetCont m_targetActor{ nullptr };
+
+    QValueNetCont m_critic{ nullptr };
+    QValueNetCont m_targetCritic{ nullptr };
+
+    std::unique_ptr<torch::optim::Adam> m_actorOptimizer;
+    std::unique_ptr<torch::optim::Adam> m_criticOptimizer;
+
+  
+    int64_t m_stateDim = 0;
+    int64_t m_actionDim = 0;
+
+    size_t m_bufferCapacity = 100000;
+    size_t m_batchSize = 64;
+
+    double m_actorLearningRate = 1e-4;
+    double m_criticLearningRate = 1e-3;
+    double m_gamma = 0.99;
+    double m_tau = 0.005;
+    
+    double m_noiseStd = 0.2;
+    double m_noiseDecay = 0.9995;
+
+    double m_dbSigma = 0.01;
+};
